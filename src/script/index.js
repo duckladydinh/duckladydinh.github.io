3f420e0a387145131f6ca43fcdc9ae37a1aa6479
Lam Gia Thuan <duckladydinh@gmail.com>
2019-11-12 12:31:45 +0100

    add react and try using some components

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,18 @@
+// bootstrap
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+// personal custom style in scss
 import '/style/scss/default.scss'
+
+// !important: react classes
+import React from "react";
+import {render} from "react-dom";
+
+// main class
+import {App} from "/script/app"
+
+// rendering
+const app = document.getElementById("app");
+render(<App/>, app);
