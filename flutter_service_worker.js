6205c83a1a90b49630a4ba6ba80bb6868ec4423f
Gia Thuan Lam <duckladydinh@gmail.com>
2020-05-18 00:58:34 +0200

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,9 +8,9 @@ const RESOURCES = {
 "assets/AssetManifest.json": "424c8f952d4cfbc9775d77b8ecce7eee",
 "assets/static/Lam_Gia_Thuan_CV_3_0.pdf": "a34a1a4a3545fe79439c0fb14cae9380",
 "assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
-"index.html": "43a73ec9993b3eb663f45066b8cf987c",
-"/": "43a73ec9993b3eb663f45066b8cf987c",
-"manifest.json": "b82ae0fcb0043526c3b2b8bfb6b0c46f",
+"index.html": "8b483ba217ee49b79f3834bc3b2739d6",
+"/": "8b483ba217ee49b79f3834bc3b2739d6",
+"manifest.json": "1cac2a606521f8398d4b6f0d6028d010",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1"
 };
