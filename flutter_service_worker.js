c8b18799eac959edfedacfb2c7fee73b1c705720
Gia Thuan Lam <duckladydinh@gmail.com>
2020-06-04 20:08:02 +0200

    CM0018 (master): Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,10 +1,10 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "d94b71cc66072a73c1ca647b8f9a8958",
+  "main.dart.js": "83e257c4061d263ffa7f08e64317cd12",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "97e5b7c95304b283a477b87ca2373966",
+"assets/LICENSE": "f3018a1ff9359f5a74e7ed2fa5b5a833",
 "assets/AssetManifest.json": "884fad55829170796f7c43a61ac09be4",
 "assets/static/Lam_Gia_Thuan_CV_4_0.pdf": "255a9b7d1e7246e454643e1c8c64a000",
 "assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
