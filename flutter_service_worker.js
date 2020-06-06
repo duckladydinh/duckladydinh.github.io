0fb54979432e40bb45a48bdb9acae2478c034375
Gia Thuan Lam <duckladydinh@gmail.com>
2020-06-06 11:14:02 +0200

    CM0028 (master): Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,7 +1,7 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "802f1db94b29f7649cdb203b9e07f4ab",
+  "main.dart.js": "84b664d1b238f5a1aefd393d15c7085b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/LICENSE": "f3018a1ff9359f5a74e7ed2fa5b5a833",
