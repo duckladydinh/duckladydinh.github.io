b8e76befec1cc8afc7b2fd4c1e3af102b83d85ff
Gia Thuan Lam <duckladydinh@gmail.com>
2020-07-25 22:14:34 +0200

    CM0080 (flutter): automate CI

--- publish-to-github.js
+++ publish-to-github.js
@@ -1,6 +1,6 @@
 const pages = require('gh-pages')
 
 pages.publish('build/web', {
-    repo: 'https://github.com/duckladydinh/duckladydinh.github.io.git',
+    repo: 'git@github.com:duckladydinh/duckladydinh.github.io.git',
     branch: 'master'
 }, null);
